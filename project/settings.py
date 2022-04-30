aa2baeee0a29a489752175d9b1173fb0dfbd4790
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-04-29 21:19:31 -0300

    Configurando variáveis de ambiente no settings para a Secret Key e o Debug.

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)yf#^mo#-)&07-2gie4cjyvf8ps1kagcq6dz5oh7)gd1d9@-%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 1 if os.environ.get('DEBUG') == '1' else 0
 
 ALLOWED_HOSTS = []
 
