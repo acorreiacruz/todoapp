dac91d8cf64229c28e36c2cba71a382656cc758e
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-05-26 12:08:19 -0300

    Alterando o model e adicionando os campos de criação e atualização.

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Tarefa(models.Model):
 
     title = models.CharField(max_length=200, null=False, blank=False)
-    done = models.BooleanField()
     description = models.TextField(null= False, blank= False, default="Descricao")
-
+    done = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self) -> str:
         return self.title
 
