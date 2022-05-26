5633620b39d645732d6f7eea2ed36a83bb296ffe
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-05-26 10:15:49 -0300

    Alterando o model e adicionando um campo de descricao.

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,7 @@ class Tarefa(models.Model):
 
     title = models.CharField(max_length=200, null=False, blank=False)
     done = models.BooleanField()
+    description = models.TextField(null= False, blank= False, default="Descricao")
 
     def __str__(self) -> str:
         return self.title
