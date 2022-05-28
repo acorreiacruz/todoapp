79293d1ffd2eca75c294188ae0fb11b4a9dbaa7a
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-05-27 21:24:39 -0300

    Criando o arquivo forms.py e criando o formlário de cadastro de uma tarefa utilizando a classe ModelForm.

---
+++ todo/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Tarefa
+
+
+class Cadastrar(forms.ModelForm):
+
+    class Meta:
+        model = Tarefa
+        fields = ['title', 'description']
+    
