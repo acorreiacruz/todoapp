b52c2e665845252e4776d0ab9d9780b724804237
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-06-08 10:52:56 -0300

    Alterando o max_length do campo title, no model, para 150

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Tarefa(models.Model):
 
-    title = models.CharField(max_length=120, null=False, blank=False)
+    title = models.CharField(max_length=150, null=False, blank=False)
     description = models.CharField(max_length=300, null=False, blank=False)
-    done = models.BooleanField()
+    done = models.BooleanField(null=True, default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
