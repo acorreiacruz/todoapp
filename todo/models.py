414151743fda74837655e61e541bc6bac0436e6b
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-06-08 09:17:21 -0300

    Alterando o tipo do campo description para CharField e determiando o max_length

--- todo/models.py
+++ todo/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Tarefa(models.Model):
 
-    title = models.CharField(max_length=200, null=False, blank=False)
-    description = models.TextField(null= False, blank= False, default="Descricao")
+    title = models.CharField(max_length=120, null=False, blank=False)
+    description = models.CharField(max_length=300,null= False, blank= False,)
     done = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.title
 
