29d50603dc0b24db9013879cfb891e44e8f94db7
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-06-27 11:20:08 -0300

    Consertando a lógica das views de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import CadastrarForm, LoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def cadastrar(request):
@@ -31,8 +33,7 @@ def cadastrar_validar(request):
 
 
 def login_view(request):
-    dados_form = request.session.get('dados_form', None)
-    form = LoginForm(dados_form)
+    form = LoginForm()
     return render(request, 'usuarios/pages/login.html', context={
         'form': form,
         'form_action': reverse('usuarios:login_validar')
@@ -43,9 +44,15 @@ def login_view_validar(request):
     if not request.POST:
         raise Http404()
 
-    dados_form = request.POST
-    request.session['dados_form'] = dados_form
-    form = LoginForm(dados_form)
+    form = LoginForm(request.POST)
 
     if form.is_valid():
-        ...
+        usuario_autenticado = authenticate(
+            email=form.cleaned_data.get('email',''),
+            password=form.cleaned_data.get('password','')
+        )
+
+        if usuario_autenticado is not None:
+
+
+
