7b0006cdccfabd9fd9e48c812f3e3547a19b0d13
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-06-27 09:32:50 -0300

    Criando o formulário de login.

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -147,3 +147,33 @@ class Cadastrar(forms.ModelForm):
             'password',
             'password_confirmation'
         ]
+
+
+class Login(forms.Form):
+    email = forms.CharField(
+        required=True,
+        label='E-mail',
+        help_text='Insira o seu endereço de e-mail já cadastrado',
+        error_messages={
+            'required': 'Campo obrigatório, por favor insira o seu e-mail'
+        },
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': 'Ex.: jhondoe@email.com'
+            }
+        )
+    )
+
+    password = forms.CharField(
+        required=True,
+        label='Senha',
+        help_text='Insira a senha da sua conta',
+        error_messages={
+            'required': 'Campo obrigatório, por favor insira a seu senha'
+        },
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Senha da conta'
+            }
+        )
+    )
