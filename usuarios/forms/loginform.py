8049852fc11247b4e89651cbdcf73cd3e11b246d
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-06-27 13:38:24 -0300

    Removendo os atributos desnecessários do login form.

--- usuarios/forms/loginform.py
+++ usuarios/forms/loginform.py
@@ -6,9 +6,6 @@ class LoginForm(forms.Form):
         required=True,
         label='E-mail',
         help_text='Insira o seu endereço de e-mail já cadastrado',
-        error_messages={
-            'required': 'Campo obrigatório, por favor insira o seu e-mail'
-        },
         widget=forms.EmailInput(
             attrs={
                 'placeholder': 'Ex.: jhondoe@email.com'
@@ -20,9 +17,6 @@ class LoginForm(forms.Form):
         required=True,
         label='Senha',
         help_text='Insira a senha da sua conta',
-        error_messages={
-            'required': 'Campo obrigatório, por favor insira a seu senha'
-        },
         widget=forms.PasswordInput(
             attrs={
                 'placeholder': 'Senha da conta'
