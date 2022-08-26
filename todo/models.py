6a7eb815e0d1f1e331626f149e1f700e712fdf69
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-26 16:41:49 -0300

    Consertando o relacionamento do model Tarefa.

--- todo/models.py
+++ todo/models.py
@@ -7,7 +7,7 @@ class Tarefa(models.Model):
     title = models.CharField(max_length=150, null=False, blank=False)
     description = models.CharField(max_length=300, null=False, blank=False)
     done = models.BooleanField(null=True, default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
