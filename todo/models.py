dc4efcb52f23c15955888f05ca719454fdef56e1
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-26 16:35:14 -0300

    Atrelando um usuário a uma tarefa no model.

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tarefa(models.Model):
@@ -6,6 +7,7 @@ class Tarefa(models.Model):
     title = models.CharField(max_length=150, null=False, blank=False)
     description = models.CharField(max_length=300, null=False, blank=False)
     done = models.BooleanField(null=True, default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
