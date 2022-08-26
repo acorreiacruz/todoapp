edf5e7ad91dad2efb59de7715e1bc0dd03f448ef
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-26 16:35:50 -0300

    Atualizando o admin do model Tarefa.

--- todo/admin.py
+++ todo/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Tarefa
 
+
 class TarefaModelAdmin(admin.ModelAdmin):
-    list_fields = ('titulo','feito')
-    
+    list_fields = ('id', 'titulo', 'feito', 'user')
+
+
 admin.site.register(Tarefa, TarefaModelAdmin)
