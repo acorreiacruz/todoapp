318143eaaaf02effc0bc636b654d464a0438bb2e
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-26 21:59:46 -0300

    Configurando a classe de Renderer do DRF

--- project/settings.py
+++ project/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJANGO_REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
