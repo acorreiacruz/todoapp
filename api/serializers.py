bc78edd070690dc73a0769882139ed38dccaa565
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-29 10:12:32 -0300

    Adicionando o campo done do model ao serializer.

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from todo.models import Tarefa
 class TarefaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tarefa
-        fields = ['id', 'title', 'description', 'author']
+        fields = ['id', 'title', 'description', 'done', 'author']
