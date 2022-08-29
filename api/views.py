d10f9bd76254fa86a5166f572841a732cfadd0e5
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-29 08:35:44 -0300

    Definindo as classes de permissões da ModelViewSet.

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from todo.models import Tarefa
 from .serializers import TarefaSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
 
 
 class TarefaModelViewSet(viewsets.ModelViewSet):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
     http_method_names = ['get', 'post', 'patch', 'delete', 'options', 'head']
+    permission_classes = [IsAuthenticated]
