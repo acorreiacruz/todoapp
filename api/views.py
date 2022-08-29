fec778f47b2e31c4577e4aadfdc381ed171a59b6
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-29 08:28:27 -0300

    Completando a ViewSet da Tarefa.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
-from ..todo.models import Tarefa
+from todo.models import Tarefa
+from .serializers import TarefaSerializer
 
 
 class TarefaModelViewSet(viewsets.ModelViewSet):
-    queryset = Tarefa
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
     http_method_names = ['get', 'post', 'patch', 'delete', 'options', 'head']
