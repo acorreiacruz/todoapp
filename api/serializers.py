8b3efdf4e7ed7cdd3c5d205e5f24a56302bac924
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-29 22:13:06 -0300

    Criando um serializer para o model User.

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from todo.models import Tarefa
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 
 class TarefaSerializer(serializers.ModelSerializer):
