acb7638aaaae4ba2ec9fa699b1ebd3ba0db70d97
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-22 22:25:37 -0300

    Reformatando as urls do app todo.

--- todo/urls.py
+++ todo/urls.py
@@ -4,24 +4,44 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-    path('', views.listar_tarefas, name='listar'),
+    path(
+        '',
+        views.listar_tarefas,
+        name='listar'
+    ),
     path(
         'tarefas/detalhar/<int:pk>/',
         views.detalhar_tarefas,
         name="detalhar"
     ),
-    path('tarefas/excluir/<int:pk>/', views.excluir_tarefa, name='excluir'),
-    path('tarefas/editar/<int:pk>/', views.editar_tarefas, name="editar"),
+    path(
+        'tarefas/excluir/<int:pk>/',
+        views.excluir_tarefa,
+        name='excluir'
+    ),
+    path(
+        'tarefas/editar/<int:pk>/',
+        views.editar_tarefas,
+        name="editar"
+    ),
     path(
         'tarefas/finalizar/<int:pk>/',
         views.finalizar_tarefa,
         name="finalizar"
     ),
-    path('tarefas/cadastrar/', views.cadastrar_tarefas, name='cadastrar'),
+    path(
+        'tarefas/cadastrar/',
+        views.cadastrar_tarefas,
+        name='cadastrar'
+    ),
     path(
         'tarefas/cadastrar/validar/',
         views.cadastrar_tarefas_validar,
         name='cadastrar_validar'
     ),
-    path('tarefas/buscar/', views.buscar_tarefas, name='buscar'),
+    path(
+        'tarefas/buscar/',
+        views.buscar_tarefas,
+        name='buscar'
+    ),
 ]
