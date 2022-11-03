bb826ec80cfc5fa9550c92d3b12b2bf086e03958
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-11-03 08:36:36 -0300

    FBV do site.

--- todo/views/site.py
+++ todo/views/site.py
@@ -1,28 +1,47 @@
-from django.urls import reverse
-from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect, render
+import os
 from ..models import Tarefa
 from ..forms import Cadastrar
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import Http404
+from utils.pagination import criar_paginacao
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, redirect, render
+
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def get_tarefa_object(pk):
     return Tarefa.objects.filter(pk=pk)
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def listar_tarefas(request):
     tarefas = Tarefa.objects.filter(
         done=False,
+        author=request.user
     ).order_by('title')
 
+    pagina_objeto, range_de_paginacao = criar_paginacao(
+        request,
+        tarefas,
+        PER_PAGE,
+        quantidade_de_paginas=4
+    )
+
     return render(request, "todo/pages/list.html", context={
-        "tarefas": tarefas,
+        "tarefas": pagina_objeto,
+        "range_de_paginacao": range_de_paginacao
     })
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def detalhar_tarefas(request, pk):
     tarefa = get_object_or_404(
         Tarefa.objects.all(),
-        id=pk
+        id=pk,
+        author=request.user
     )
 
     return render(request, "todo/pages/detail.html", context={
@@ -30,24 +49,33 @@ def detalhar_tarefas(request, pk):
     })
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def editar_tarefas(request, pk):
     get_tarefa_object(pk).update(
         title=request.POST.get('title'),
-        description=request.POST.get('description')
+        description=request.POST.get('description'),
+        author=request.user
     )
+
+    messages.success(request, "Tarefa editada com sucesso !")
     return redirect('/')
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def excluir_tarefa(request, pk):
     get_tarefa_object(pk).delete()
+    messages.success(request, "Tarefa excluída com sucesso !")
     return redirect('/')
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def finalizar_tarefa(request, pk):
     get_tarefa_object(pk).update(done=True)
+    messages.success(request, "Tarefa finalizada com sucesso !")
     return redirect('/')
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def cadastrar_tarefas(request):
     dados_form = request.session.get('dados_form', None)
     form = Cadastrar(dados_form)
@@ -58,6 +86,7 @@ def cadastrar_tarefas(request):
     })
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def cadastrar_tarefas_validar(request):
     if not request.POST:
         raise Http404()
@@ -69,11 +98,14 @@ def cadastrar_tarefas_validar(request):
     if form.is_valid():
         form.save()
         del(request.session['dados_form'])
+        messages.success(request, "Nova tarefa criada com sucesso !")
         return redirect('/')
 
+    messages.error(request, "Erro ao tentar criar a tarefa !")
     return redirect('todo:cadastrar')
 
 
+@login_required(login_url='usuarios:login', redirect_field_name='next')
 def buscar_tarefas(request):
 
     q = request.GET.get('q', '')
@@ -82,6 +114,16 @@ def buscar_tarefas(request):
         title__icontains=q
     )
 
-    return render(request, 'todo/pages/list.html', context={
-        'tarefas': tarefas,
+    pagina_objeto, range_de_paginacao = criar_paginacao(
+        request,
+        tarefas,
+        PER_PAGE,
+        quantidade_de_paginas=4
+    )
+
+    return render(request, 'todo/pages/search.html', context={
+        "termo_buscado": q,
+        "tarefas": pagina_objeto,
+        "range_de_paginacao": range_de_paginacao,
+        "query_string_de_busca": f"q={q}&"
     })
